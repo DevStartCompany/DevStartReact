8e9da77d28c6d519d2d938f46edb8f01b8b0b92e
DevStartCompany <devstartcompany@gmail.com>
2023-05-01 00:51:08 -0300

    tst

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./Pages/Home/Home";
-
+import { Login } from "./Pages/login/Login"
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
