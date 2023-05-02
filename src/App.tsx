e89918388bf5f8703e35a3baf5fd031c4a69db77
thalles-victor <thalles.victor2012@gmail.com>
2023-05-02 05:41:03 -0300

    :bug: fix: remove hashrouter no used to deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./Pages/Home/Home";
 import { SignUp } from "./Pages/SignUp/SignUp";
 import { PageNotFound } from "./Pages/404";
