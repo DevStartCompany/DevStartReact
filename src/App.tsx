c0b7d1c20b4b34a53156547753cecdfc8205e8fc
thalles-victor <thalles.victor2012@gmail.com>
2023-05-06 07:44:19 -0300

    :recycle: refactor: Move browser router to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { Home } from "./Pages/Home/Home";
 import { SignUp } from "./Pages/SignUp/SignUp";
 import { PageNotFound } from "./Pages/404";
@@ -6,12 +6,14 @@ import { SignIn } from "./Pages/SignIn";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/criar-conta" element={<SignUp />} />
-      <Route path="/entrar" element={<SignIn />} />
-      <Route path="*" element={<PageNotFound />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/criar-conta" element={<SignUp />} />
+        <Route path="/entrar" element={<SignIn />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
