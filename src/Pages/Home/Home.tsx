92912cb550ce3f0ea960ab9da7ed7d1a0208fe1f
DevStartCompany <devstartcompany@gmail.com>
2023-05-06 23:27:34 -0300

    :sparkles: Novas atualizações

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -3,14 +3,16 @@ import userLogo from "../../assets/user.png";
 import { BtnNavigation } from "../../components/btn-navigation";
 import { Stacks } from "../../components/stacks";
 import * as PhosphorIcons from "phosphor-react";
-import { useNavigate } from "react-router-dom";
+import  NestImage  from "../../assets/nest-og.png"
+
+//import { useNavigate } from "react-router-dom";
 
 export function Home() {
-const navigate = useNavigate()
-const token = localStorage.getItem("token");
-if (!token) {
-  navigate('/entrar')
-}
+//const navigate = useNavigate()
+//const token = localStorage.getItem("token");
+//if (!token) {
+ // navigate('/entrar')
+//}
 
 
   return (
@@ -24,18 +26,18 @@ if (!token) {
           </div>
         </div>
         <div className="btn-navigation">
-          <BtnNavigation content="Principal" />
-          <BtnNavigation content="Cursos pendentes" />
-          <BtnNavigation content="Perfil" />
-          <BtnNavigation content="Configurações" />
-          <BtnNavigation content="Logout" />
+          <BtnNavigation icon={<PhosphorIcons.ShieldCheck size={32}/>} content="Principal" />
+          <BtnNavigation icon={<PhosphorIcons.MonitorPlay size={32}/>} content="Cursos pendentes" />
+          <BtnNavigation icon={<PhosphorIcons.UserCircleGear size={32}/>} content="Perfil" />
+          <BtnNavigation icon={<PhosphorIcons.Gear size={32}/>} content="Configurações" />
+          <BtnNavigation icon={<PhosphorIcons.SignOut size={32}/>} content="Logout" />
         </div>
       </section>
 
       <section className="mid-sec">
         <div className="flex mid2">
-          <img className="userlogo" src={userLogo} />
-          <div className="userinfo-2">
+            <img className="userlogo" src={userLogo} />
+            <div className="userinfo-2">
             <p className="hello">Bem vindo,</p>
             <p className="username2">NOME</p>
           </div>
@@ -69,10 +71,45 @@ if (!token) {
             icon={<PhosphorIcons.ShieldCheck size={32} />}
           />
         </div>
+
+
+        <div className="courses flex space-around">
+            <div className="course">
+              <img className="course-image cursor" src={NestImage}></img>
+              
+              <div className="course-text">
+                <div className="flex">              
+                  <PhosphorIcons.Users size={24} />
+                  <p className="students ">PuxarBanco Estudantes</p>
+                </div>
+
+                  <h1 className="course-name cursor underline">Curso de PuxarBanco do Básico ao Avançado</h1>
+                    <div className="teacher">
+                      <img className="teacher-image cursor" src={userLogo}></img>
+                      <p className="teacher-name cursor underline">PuxarBanco (Professor)</p>
+                    </div>
+                  </div>
+
+            </div>
+
+            <div className="curso-2">
+
+            </div>
+
+            <div className="curso-3">
+
+            </div>
+        </div>
       </section>
 
-      <section className="right-sec"></section>
+      <section className="right-sec">
+
+
+      </section>
     </section>
+
+    
   );
 
 }
+
--- src/components/btn-navigation/index.tsx
+++ src/components/btn-navigation/index.tsx
@@ -1,16 +1,19 @@
+import { ReactNode } from "react"
 import "./styles.css"
 
 interface BtnNavigationProps{
     img?:string
     content:string
     alt?:string
+    icon:ReactNode
 }
 
-export function BtnNavigation({img, content, alt=""}:BtnNavigationProps) {
+export function BtnNavigation({img, content, alt="", icon}:BtnNavigationProps) {
     return (
         <div className="btn-navigation">
             {img? <img src={img} alt={alt} /> : null}
-            <h1 className="btn-1">{content}</h1>
+        
+            <h1 className="btn-1">{icon}{content}</h1>
         </div>
     )
 } 
